859646d84dda26f592d3c520a7a3bacf77a8f11a
Bhautik Hirapara <bhautikhirpara144@gmail.com>
2024-02-19 17:37:20 +0530

    Login/Signup + Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import { Counter } from "./features/counter/Counter";
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from "./pages/LoginPage";
+import Signup from "./features/auth/components/Signup";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      {/* <Counter></Counter> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
