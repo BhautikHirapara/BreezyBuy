58a9e7bc814ecb20b98d206beeba0603c3237836
Bhautik Hirapara <bhautikhirpara144@gmail.com>
2024-02-22 15:22:58 +0530

    JSON-server/ Products Filters / JSON-server / Setup Redux

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ProductDetail from '../features/product-list/components/ProductDetail'
+import NavBar from '../features/navbar/Navbar'
 
 function ProductDetailPage() {
   return (
     <div>
+        <NavBar></NavBar>
         <ProductDetail></ProductDetail>
     </div>
   )
